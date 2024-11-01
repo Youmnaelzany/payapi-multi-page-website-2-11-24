508bbb2b2f3d8fccf15d4b3b835bd2554bb5952d
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-11-02 01:02:14 +0200

    setting uo the project & finished footer component

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"Dark-Pink": '#BA4270',
+			"Link-Water-White": '#E1E1E1',
+			"San-Juan-Blue": '#36536B',
+			"Mirage-Blue": '#1B262F',
+			"Charm-Pink": '#DA6D97',
+			"Light-San-Juan-Blue": '#6C8294',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Public Sans', 'sans-serif'],
+				serif: ['DM Serif Display', 'serif'],
+			},
+		},
+	},
+	plugins: [],
+}
